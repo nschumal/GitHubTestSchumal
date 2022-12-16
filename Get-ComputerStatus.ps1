ec305432e7317ba054085314c109e1c711e6f76b
Nolan Schumal <nschumal@madisoncollege.edu>
2022-12-15 20:55:52 -0600

    Step 5

--- Get-ComputerStatus.ps1
+++ Get-ComputerStatus.ps1
@@ -1,61 +1,55 @@
 #Nolan Schumal
-#Online
-#Script for outputting OS name build and version to report.txt
-
-#For each command you have the following 3 lines:
-#1 is a comment line in your script that provides information about what your script is doing.  Example: "1. Outputing information about installed hotfixes"
-#2 is header infomration for your report file. Example: Write-Output 'Header/Title of Output' | out-file report.txt
-#3 is the command itself
-#Example of a single command:
-#Ex1. Outputing Servcies that are running
-#Write-Output "#1: Outputing Services that are running" | Out-File report.txt
-#Get-Service | Where-Object Status -eq 'Running' | Out-File report.txt -Append
+#Online Infastructure Automation
+#12/12/2022
+#Script for gathering information on a system
 
 
 param (
     [Parameter (Mandatory=$True)]
     [string] $computername
 )
-
+#Os info
 Get-CIMinstance win32_OperatingSystem -ComputerName $computername|
 select -property Caption,Buildnumber,Version|
-Out-file -FilePath .\report.txt
-
+Out-file -FilePath .\SchumalFall2022.txt -Encoding utf8
+#Processor info
 Get-CimInstance Win32_Processor -ComputerName $computername|
 Select-Object -Property MaxClockSpeed,DeviceID,Name|
-Out-file -FilePath .\report.txt -Append
-
+Out-file -FilePath .\SchumalFall2022.txt -Append
+#IP info
 Get-CimInstance Win32_NetworkAdapterConfiguration -ComputerName $computername|
 Select-Object -Property IPAddress, IPSubnet, DefaultIPGateway, DHCPEnabled|
-Out-file -FilePath .\report.txt -Append
-
+Out-file -FilePath .\SchumalFall2022.txt -Append
+#DNS info
 Get-dnsclientserveraddress -cimsession $computername|
 Select-Object -Property ServerAddresses|
-Out-file -FilePath .\report.txt -Append
-
+Out-file -FilePath .\SchumalFall2022.txt -Append
+#Memory in GB
 Get-CimInstance Win32_PhysicalMemory -ComputerName $computername|
 Select-Object -Property @{
 label='Capacity (GB)'
 expression={($_.Capacity/1GB).ToString('F2')}
 }|
-Out-file -FilePath .\report.txt -Append
-
+Out-file -FilePath .\SchumalFall2022.txt -Append
+#Free space in C:
 Get-CimInstance Win32_LogicalDisk -ComputerName $computername|
 Where-Object -Property DeviceID -eq 'C:' |
 Select-Object -Property @{
 label='FreeSpace (GB)'
 expression={($_.FreeSpace/1GB).ToString('F2')}
 }|
-Out-file -FilePath .\report.txt -Append
-
+Out-file -FilePath .\SchumalFall2022.txt -Append
+#Last boot up time
 Get-CimInstance Win32_OperatingSystem -ComputerName $computername|
 Select-Object -Property CSName,LastBootUpTime|
-Out-file -FilePath .\report.txt -Append
-
+Out-file -FilePath .\SchumalFall2022.txt -Append
+#last user login
 Get-CimInstance Win32_computersystem -ComputerName $computername|
 Select-Object -Property UserName|
-Out-file -FilePath .\report.txt -Append
+Out-file -FilePath .\SchumalFall2022.txt -Append
+#all users
 
+#installed apps
 Get-CimInstance Win32_Product -ComputerName $computername|
 Select-Object -Property Name,Vendor,Version|
-Out-file -FilePath .\report.txt -Append
+Out-file -FilePath .\SchumalFall2022.txt -Append
